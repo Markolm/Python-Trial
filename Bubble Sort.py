db93778370bf21d5f43d367e10c7f2b5bf6c044d
Markolm <markleonov@gmail.com>
2015-02-22 13:16:44 +0100

    Bubble Sorting
    Very easy. Which one is better?

---
+++ Bubble Sort.py
@@ -0,0 +1,20 @@
+import random
+import time
+order = []
+print("The script will generate ten random numbers from 1 to 10")
+time.sleep(2)
+for number in range(10):
+    order.append(random.randint(1,9))
+print(order)
+time.sleep(1)
+print("And now will list them in order, from smallest to largest")
+time.sleep(2)
+for turn in range(10):
+    for intval in range(9):
+        if order[intval]>order[intval+1]:
+            num1 = order[intval]
+            num2 = order [intval+1]
+            order[intval] = num2
+            order [intval+1] = num1
+print(order)
+
